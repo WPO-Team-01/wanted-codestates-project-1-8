abdbfcecf9791a02b2bd308e10cbf75a00adc5f6
김범석 <kqjatjr@gmail.com>
2022-02-28 16:46:59 +0900

    리덕스 기본 셋팅

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,9 @@
-console.log("HELL");
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { ForestApi } from "./query/ForestApi";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+setupListeners(store.dispatch);
